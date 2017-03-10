0477e842696404abb553dfa005e2120233b95a37
Maia <raafamaia@gmail.com>
2017-03-09 21:16:55 -0300

    bouncing messages

--- messages/message.js
+++ messages/message.js
@@ -1,20 +1,51 @@
-function Message(word, x, y){
-    this.x = x;
-    this.y = y;
+function Message(word, m, x, y){
+    this.word = word;
+    this.mass = m;
+    this.position = createVector(x,y);
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+    
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+    
+    this.size = random(40, 60);
     
     this.setStyle = function(){
-        textSize(20);
-        fill(255, 255, 255, 90);
-    };
-    
-    this.display = function(){
-        this.setStyle();
-        text(word, this.x, this.y, this.radius);
-    };
-    
-    this.move = function(){
-        this.x = this.x + random(-1, 1);
-        this.y = this.y + random(-1, 1);
+        textSize(this.size);
+        stroke(this.g, this.r, this.b);
+        strokeWeight(7);
+        fill(this.r, this.g, this.b);
     };
     
 }
+
+// Newton's 2nd law: F = M * A
+// or A = F / M
+Message.prototype.applyForce = function(force) {
+  var f = p5.Vector.div(force,this.mass);
+  this.acceleration.add(f);
+};
+  
+Message.prototype.update = function() {
+  // Velocity changes according to acceleration
+  this.velocity.add(this.acceleration);
+  // position changes by velocity
+  this.position.add(this.velocity);
+  // We must clear acceleration each frame
+  this.acceleration.mult(0);
+};
+
+Message.prototype.display = function() {
+    this.setStyle();
+    text(this.word, this.position.x,this.position.y, this.mass*16);
+};
+
+// Bounce off bottom of window
+Message.prototype.checkEdges = function() {
+  if (this.position.y > (height - this.mass*8)) {
+    // A little dampening when hitting the bottom
+    this.velocity.y *= -0.9;
+    this.position.y = (height - this.mass*8);
+  }
+};
--- messages/sketch.js
+++ messages/sketch.js
@@ -1,24 +1,58 @@
 var messages = [];
+var phrases = [["MANDA", "NUDES"], ["OI", "VAMO", "BEJA?"], ["COMO", "VAI", "VOSE", "CHEROSA?"], ["PAGUE", "SUAS", "APOSTAS", "PARSA"]];
+
+var currentPhrase = [];
+var currentIndexPhrase = 0;
+var currentIndexWord = 0;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    
+    currentPhrase = phrases[0];
 }
 
 function draw() {
     background(0);
     
-    if(messages.length > 50){
-        messages.splice(0, 1);
-    }
-    
     for(let message of messages){
-        message.move();
+        // Gravity is scaled by mass here!
+        var gravity = createVector(0, 0.1 * message.mass);
+        // Apply gravity
+        message.applyForce(gravity);
+
+        // Update and display
+        message.update();
         message.display();
+        message.checkEdges();
     }
 }
 
 function mousePressed(){
-    messages.push(new Message("teste", mouseX, mouseY));
+    
+    //Check if the phrase is over
+    if(currentIndexWord >= currentPhrase.length) {
+        currentIndexPhrase += 1;
+        currentPhrase = phrases[currentIndexPhrase];
+        currentIndexWord = 0;
+        
+        reset();
+    }
+    
+    //Check if it's the last phrase
+    if(currentIndexPhrase > phrases.length -1){
+        currentIndexPhrase = 0;
+        currentIndexWord = 0;
+        
+        currentPhrase = phrases[currentIndexPhrase];
+    }
+    
+    messages.push(new Message(currentPhrase[currentIndexWord], random(0.5, 3), mouseX, mouseY));
+    
+    currentIndexWord += 1;
+}
+
+function reset(){
+     messages=[];
 }
 
 
